f6cd03ff53cc5fdf5908f45c56829bb221874ea6
akilinjamam <akilinjamam@gmail.com>
2025-08-03 11:35:46 -0500

    file-updated

--- src/component/GlassCardsGallary.tsx
+++ src/component/GlassCardsGallary.tsx
@@ -19,7 +19,7 @@ const GlassCardsGallary:FC = () => {
                 {
                     glassData?.map(({colorCount, title, model, price, tag, image }: GlassCardProps, index: number) => <GlassCard colorCount={colorCount} image={image} tag={tag} price={price} title={title} model={model} key={index}/> )
                 }
-               <div className="pointer-events-none absolute top-0 right-10 h-full w-32 bg-gradient-to-l from-blue-50/100 to-blue-50/0 z-10" />
+               <div className="pointer-events-none absolute top-0 right-10 sm:right-8 h-full w-32 bg-gradient-to-l from-blue-50/100 to-blue-50/0 z-10" />
             </div>
              <Image className='cursor-pointer ' src={arrowRight} alt='right-arrow' onClick={() => handleNavigation('left')}/>
         </div>
