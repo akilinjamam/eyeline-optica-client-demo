4b68b0c60f271a308dc5f40c19b1ffa1d8904b90
akilinjamam <akilinjamam@gmail.com>
2025-08-03 11:26:45 -0500

    file-updated-done

--- src/component/GlassCardsGallary.tsx
+++ src/component/GlassCardsGallary.tsx
@@ -14,14 +14,14 @@ const GlassCardsGallary:FC = () => {
 
     return (
         <div className='flex items-center justify-around lg:w-[1200px] md:w-[80%] mx-auto gap-5 relative'>
-            <Image className='cursor-pointer inline-block mx-3' src={arrowLeft} alt='left-arrow' onClick={() => handleNavigation('right')}/>
+            <Image className='cursor-pointer' src={arrowLeft} alt='left-arrow' onClick={() => handleNavigation('right')}/>
             <div style={{userSelect:'none'}}  ref={parentRef} className='w-[2600px] h-[300px] mx-auto flex items-center  gap-6 mt-10 overflow-x-hidden scroll-smooth  px-2 '>
                 {
                     glassData?.map(({colorCount, title, model, price, tag, image }: GlassCardProps, index: number) => <GlassCard colorCount={colorCount} image={image} tag={tag} price={price} title={title} model={model} key={index}/> )
                 }
-               <div className="pointer-events-none absolute top-0 right-15 h-full w-32 bg-gradient-to-l from-blue-50/100 to-blue-50/0 z-10" />
+               <div className="pointer-events-none absolute top-0 right-10 h-full w-32 bg-gradient-to-l from-blue-50/100 to-blue-50/0 z-10" />
             </div>
-             <Image className='cursor-pointer inline-block mx-3' src={arrowRight} alt='right-arrow' onClick={() => handleNavigation('left')}/>
+             <Image className='cursor-pointer ' src={arrowRight} alt='right-arrow' onClick={() => handleNavigation('left')}/>
         </div>
     );
 };
