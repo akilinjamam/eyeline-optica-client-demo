cb16878aa58cca8ae73fc9a0a67c2fcabba00b09
akilinjamam <akilinjamam@gmail.com>
2025-08-06 01:42:06 +0600

    product-detail-updated

--- src/app/singleproduct/[id]/page.tsx
+++ src/app/singleproduct/[id]/page.tsx
@@ -8,18 +8,19 @@ import ImagePart from "@/component/UI/productDetail/ImagePart";
  const SingleProduct = async ({ params }: any) => {
   const { id } = params; // destructure inside function body
   const product = glassData.find((p) => p.id === Number(id));
-  console.log(product)
-
+ 
   if (!product) return null;
 
   return (
-    <div className='w-[1250px] mx-auto flex items-center'>
-      <div className='w-[55%]'>
-        <ImagePart product={product}/>
-      </div>
-      <div className='w-[45%]'>
-        <DetailPart />
-      </div>
+    <div className="w-full bg-blue-50">
+        <div className='w-full md:w-[90%] lg:w-[1250px] mx-auto md:flex lg:flex items-center border-y border-gray-400 flex-wrap'>
+            <div className='sm:w-full md:w-[55%] lg:w-[55%]'>
+              <ImagePart product={product}/>
+            </div>
+            <div className='sm:w-full md:w-[45%] lg:w-[45%]'>
+              <DetailPart />
+            </div>
+        </div>
     </div>
   );
 };
