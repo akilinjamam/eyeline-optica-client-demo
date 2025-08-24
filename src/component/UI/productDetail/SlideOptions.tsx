9a57b5e1d483368f0de54f86117e3d70d5f3adcb
akilinjamam <akilinjamam@gmail.com>
2025-08-24 17:49:50 +0600

    file-updated

--- src/component/UI/productDetail/SlideOptions.tsx
+++ src/component/UI/productDetail/SlideOptions.tsx
@@ -139,7 +139,51 @@ const SlideOptions = () => {
             )}
             {current.type === "Enter Power Manually" && (
               <>
-                Enter Power Manually
+                <div className='w-[98%] mx-2 rounded-md px-2 '>
+                    <p className='font-semibold text-xl'>Enter Power Manually</p>
+                    <br />
+                    <div className='w-full bg-gray-100 rounded-md p-2'>
+                        <p><span><input type="checkbox" name="" id="" /></span> I have same power for both eyes</p>
+                        <br />
+                        <p><span><input type="checkbox" name="" id="" /></span> I have cylindrical power</p>
+
+                        <div className='grid grid-cols-3'>
+                            <div>
+                                <br />
+                                <p className='font-semibold'>Power</p>
+                                <br />
+                                SPH
+                            </div>
+                            <div>
+                                <br />
+                                <p className='font-semibold'>LEFT (OS)</p>
+                                <br />
+                                <select style={{width:'90px'}} name="" id="" className='w-[100px] border border-gray-400 rounded-md p-1'>
+                                    <option value="">Select</option>
+                                </select>
+                            </div>
+                            <div>
+                                <br />
+                                <p className='font-semibold'>RIGHT (OD)</p>
+                                 <br />
+                                  <select style={{width:'90px'}} name="" id="" className='w-[100px] border border-gray-400 rounded-md p-1'>
+                                      <option value="">Select</option>
+                                  </select>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                    <p className='font-semibold text-xl'>Whose Prescription is this</p>
+                    <div>
+                        <br />
+                        <input className='w-full rounded-md p-4 border border-gray-400' type="text" name="" id="" placeholder='Name *' />
+                        <br /><br />
+                        <input className='w-full rounded-md p-4 border border-gray-400' type="text" name="" id="" placeholder='Phone Number *' />
+                    </div>
+                    <div className='w-full text-center mt-6 text-green-800'>
+                        <p>Can’t find your power, Call +8801 99088765675</p>
+                    </div>
+                </div>
               </>
             )}
             {current.type === "Upload Prescription" && (
