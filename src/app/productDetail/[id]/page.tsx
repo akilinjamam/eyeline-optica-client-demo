f4f9a7f9d35d2d1a7693e8df04a06f2c22042813
akilinjamam <akilinjamam@gmail.com>
2025-09-28 23:23:59 +0600

    revalidation-updated

--- src/app/productDetail/[id]/page.tsx
+++ src/app/productDetail/[id]/page.tsx
@@ -13,7 +13,7 @@ async function getSingleProduct(id: string) {
 
   const response = await fetch(
     `https://eyeline-optica-server.onrender.com/api/v1/products/get-single-product/${id}`,{
-        next: {revalidate: 6000}
+        next: {revalidate: 60}
     },
 
   );
