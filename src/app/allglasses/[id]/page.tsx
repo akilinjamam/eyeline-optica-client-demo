c63f1b24d2999311443e7f2778eac4f5bfa9580b
akilinjamam <akilinjamam@gmail.com>
2025-09-28 19:41:30 +0600

    file-updated

--- src/app/allglasses/[id]/page.tsx
+++ src/app/allglasses/[id]/page.tsx
@@ -1,9 +1,9 @@
-import Footer from "@/component/Footer";
-import ShopByFrameShape from "@/component/ShopByFrameShape";
-import TopFooter from "@/component/TopFooter";
-import DetailPart from "@/component/UI/productDetail/DetailPart";
-import ImagePart from "@/component/UI/productDetail/ImagePart";
-import RegardingInfo from "@/component/UI/productDetail/RegardingInfo";
+// import Footer from "@/component/Footer";
+// import ShopByFrameShape from "@/component/ShopByFrameShape";
+// import TopFooter from "@/component/TopFooter";
+// import DetailPart from "@/component/UI/productDetail/DetailPart";
+// import ImagePart from "@/component/UI/productDetail/ImagePart";
+// import RegardingInfo from "@/component/UI/productDetail/RegardingInfo";
 import { TData, TFrame } from "@/ts-definition/types";
 import { notFound } from "next/navigation";
 
@@ -32,26 +32,28 @@ export default async function SingleProduct({
   if (!product?.data) return notFound();
 
   const frame = product.data;
+  console.log(frame)
 
   return (
-    <div className="w-full bg-blue-50 px-1">
-      <div className="w-full md:w-[90%] lg:w-[1250px] mx-auto md:flex lg:flex items-center border-y border-gray-400 flex-wrap">
-        <div className="sm:w-full md:w-[55%] lg:w-[55%]">
-          <ImagePart product={frame} />
-        </div>
-        <div className="sm:w-full md:w-[45%] lg:w-[45%]">
-          <DetailPart {...(frame as TFrame)} />
-        </div>
-      </div>
+    // <div className="w-full bg-blue-50 px-1">
+    //   <div className="w-full md:w-[90%] lg:w-[1250px] mx-auto md:flex lg:flex items-center border-y border-gray-400 flex-wrap">
+    //     <div className="sm:w-full md:w-[55%] lg:w-[55%]">
+    //       <ImagePart product={frame} />
+    //     </div>
+    //     <div className="sm:w-full md:w-[45%] lg:w-[45%]">
+    //       <DetailPart {...(frame as TFrame)} />
+    //     </div>
+    //   </div>
 
-      <div className="bg-blue-50 w-full md:w-[90%] lg:w-[1250px] mx-auto">
-        <RegardingInfo data={frame as TFrame} />
-      </div>
-      <div className="bg-blue-50 w-full md:w-[90%] lg:w-[1250px] mx-auto">
-        <ShopByFrameShape />
-      </div>
-      <TopFooter />
-      <Footer />
-    </div>
+    //   <div className="bg-blue-50 w-full md:w-[90%] lg:w-[1250px] mx-auto">
+    //     <RegardingInfo data={frame as TFrame} />
+    //   </div>
+    //   <div className="bg-blue-50 w-full md:w-[90%] lg:w-[1250px] mx-auto">
+    //     <ShopByFrameShape />
+    //   </div>
+    //   <TopFooter />
+    //   <Footer />
+    // </div>
+    <p>{id}</p>
   );
 }
--- src/component/UI/GlassCardAuto.tsx
+++ src/component/UI/GlassCardAuto.tsx
@@ -23,7 +23,7 @@ const GlassCardAuto:React.FC<TFrame> = ({images, name, brand, salesPrice, badge,
       </span>
 
       <div className=" mb-3 w-full flex items-center justify-center">
-        <Link href={`/singleproduct/${_id}`}>
+        <Link href={`/allglasses/${_id}`}>
           <Image
           src={images?.length && images?.length > 0 ? images?.[0] : defaultImage}
           alt={name as string}
