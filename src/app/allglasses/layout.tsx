86db14c2868af6d8029a42e645213e34b543fca9
akilinjamam <akilinjamam@gmail.com>
2025-09-30 02:32:19 +0600

    suspend added

--- src/app/allglasses/layout.tsx
+++ src/app/allglasses/layout.tsx
@@ -3,21 +3,25 @@ import TopFooter from '@/component/TopFooter';
 import SidebarOld from '@/component/UI/SideBarOld';
 import { getFrame } from '@/fetchData/fetchFrameData';
 import { TData, TFrame } from '@/ts-definition/types';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, Suspense } from 'react';
 
-const Sunglasses = async ({children}: {children:ReactNode}) => {
-    const allFrameData = await getFrame({}) as TData<TFrame[]>;
-    const data = allFrameData?.data?.data;
-     return (
-        <div className="w-full bg-blue-50 relative">
-            <div className="w-full mx-auto flex items-start">
-                <SidebarOld data={data}/>
-                {children}
-            </div>
-            <TopFooter/>
-            <Footer/>
-        </div>
-    );
+const Sunglasses = async ({ children }: { children: ReactNode }) => {
+  const allFrameData = await getFrame({}) as TData<TFrame[]>;
+  const data = allFrameData?.data?.data;
+
+  return (
+    <div className="w-full bg-blue-50 relative">
+      <div className="w-full mx-auto flex items-start">
+       
+        <Suspense fallback={<div>Loading filters...</div>}>
+          <SidebarOld data={data} />
+        </Suspense>
+        {children}
+      </div>
+      <TopFooter />
+      <Footer />
+    </div>
+  );
 };
 
 export default Sunglasses;
