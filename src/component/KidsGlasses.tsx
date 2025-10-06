383e4172e9aa7c4a7a1bf54d9c8d370e92cf3979
akilinjamam <akilinjamam@gmail.com>
2025-10-06 15:54:21 +0600

    blank

--- src/component/KidsGlasses.tsx
+++ src/component/KidsGlasses.tsx
@@ -1,15 +1,21 @@
+"use client"
 import { TContactLenseData, TFrame } from '@/ts-definition/types';
 import kidsImg from '../../public/images/kids-glass-banner.png';
 import Image from 'next/image';
-
+import arrowLeft from '../../public/images/arrow-left.png'
+import arrowRight from '../../public/images/arrow-right.png'
 import Title from './Title';
-import GlassCardsGallary from './GlassCardsGallary';
+import GlassCard from './GlassCard';
+import useWeeklyDealsScroller from '@/custom-hooks/useWeeklyDealsScroller';
+import { useRef } from 'react';
 
 const Kidsglass = ({data}:{data:TFrame[]}) => {
 
     const contactLense:TContactLenseData ={
         lense: kidsImg
     }
+    const ref = useRef(null)
+    const {handleNavigation, parentRef} = useWeeklyDealsScroller(ref)
 
     return (
         <div className='mt-8'>
@@ -18,8 +24,17 @@ const Kidsglass = ({data}:{data:TFrame[]}) => {
                 <Image className='mx-auto' src={contactLense.lense} alt='contact-lense'/>
             </div>
             <div className='w-full'>
-                <GlassCardsGallary data={data}/>
-            </div>
+                <div className='flex items-center justify-around lg:w-[1200px] md:w-[80%] mx-auto gap-5 relative'>
+                    <Image className='cursor-pointer inline-block mx-3' src={arrowLeft} alt='left-arrow' onClick={() => handleNavigation('right')}/>
+                    <div style={{userSelect:'none'}}  ref={parentRef} className='w-[2600px] h-[300px] mx-auto flex items-center  gap-6 mt-10 overflow-x-hidden scroll-smooth  px-2 '>
+                        {
+                            data?.filter((f:TFrame) => f?.biologyCategory === "kids")?.slice(0,13).map(({color, name, brand, salesPrice, badge, images,_id }: TFrame, index: number) => <GlassCard color={color} images={images} badge={badge} salesPrice={salesPrice} name={name} brand={brand} key={index} _id={_id}/> )
+                        }
+                        <div className="pointer-events-none absolute top-0 right-13 lg:right-16 h-full w-32 bg-gradient-to-l from-blue-50/100 to-blue-50/0 z-10" />
+                        </div>
+                        <Image className='cursor-pointer inline-block mx-3' src={arrowRight} alt='right-arrow' onClick={() => handleNavigation('left')}/>
+                    </div>
+                </div>
         </div>
     );
 };
--- src/component/WomenSunglass.tsx
+++ src/component/WomenSunglass.tsx
@@ -1,11 +1,18 @@
+"use client"
 import { TContactLenseData, TFrame } from '@/ts-definition/types';
 import womensImg from '../../public/images/women-glass-banner.png';
 import Image from 'next/image';
-
+import arrowLeft from '../../public/images/arrow-left.png'
+import arrowRight from '../../public/images/arrow-right.png'
 import Title from './Title';
-import GlassCardsGallary from './GlassCardsGallary';
+import GlassCard from './GlassCard';
+import { useRef } from 'react';
+import useWeeklyDealsScroller from '@/custom-hooks/useWeeklyDealsScroller';
 
 const WomenSunglass = ({data}: {data:TFrame[]}) => {
+
+    const currentRef = useRef(null);
+        const {handleNavigation, parentRef} = useWeeklyDealsScroller(currentRef)
 
     const contactLense:TContactLenseData ={
         lense: womensImg
@@ -18,7 +25,16 @@ const WomenSunglass = ({data}: {data:TFrame[]}) => {
                 <Image className='mx-auto' src={contactLense.lense} alt='contact-lense'/>
             </div>
             <div className='w-full'>
-                <GlassCardsGallary data={data}/>
+               <div className='flex items-center justify-around lg:w-[1200px] md:w-[80%] mx-auto gap-5 relative'>
+                    <Image className='cursor-pointer inline-block mx-3' src={arrowLeft} alt='left-arrow' onClick={() => handleNavigation('right')}/>
+                    <div style={{userSelect:'none'}}  ref={parentRef} className='w-[2600px] h-[300px] mx-auto flex items-center  gap-6 mt-10 overflow-x-hidden scroll-smooth  px-2 '>
+                        {
+                            data?.filter((f:TFrame) => f?.biologyCategory === "women")?.slice(0,13).map(({color, name, brand, salesPrice, badge, images,_id }: TFrame, index: number) => <GlassCard color={color} images={images} badge={badge} salesPrice={salesPrice} name={name} brand={brand} key={index} _id={_id}/> )
+                        }
+                        <div className="pointer-events-none absolute top-0 right-13 lg:right-16 h-full w-32 bg-gradient-to-l from-blue-50/100 to-blue-50/0 z-10" />
+                        </div>
+                        <Image className='cursor-pointer inline-block mx-3' src={arrowRight} alt='right-arrow' onClick={() => handleNavigation('left')}/>
+                </div>
             </div>
         </div>
     );
