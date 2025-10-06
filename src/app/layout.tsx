f2fcf3c49f7ed979bbdb24bc116dc66ed4a99d29
akilinjamam <akilinjamam@gmail.com>
2025-10-06 20:04:22 +0600

    layout-updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/component/Header";
-// import Tab from "@/component/Tab";
+import Tab from "@/component/Tab";
 
 const inter = Inter({
   variable: "--font-inter-sans",
@@ -27,7 +27,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         <Header/>
-        {/* <Tab/> */}
+        <Tab/>
         {children}
       </body>
     </html>
